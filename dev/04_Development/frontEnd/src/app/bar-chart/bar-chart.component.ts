c81bf0899a0da1797560cbd2627daa00b7b91461
Wayne Guangzhe Wen <jt20452@bristol.ac.uk>
2021-04-15 11:23:45 +0100

    barchart0415

--- dev/04_Development/frontEnd/src/app/bar-chart/bar-chart.component.ts
+++ dev/04_Development/frontEnd/src/app/bar-chart/bar-chart.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar-chart',
@@ -7,20 +9,35 @@ import {Component, OnInit} from '@angular/core';
 })
 
 export class BarChartComponent implements OnInit {
-  constructor() {
-  }
-
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: { xAxes: [{}], yAxes: [{}] }
   };
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
+  public barChartLabels: Label[] = ['', '', '', '', '', '', ''];
+  public barChartType: ChartType = 'horizontalBar';
   public barChartLegend = true;
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+
+  public barChartData: ChartDataSets[] = [
+    { data: [8, 5, 90, 4, 5, 6, 7], label: 'Votes' }
   ];
 
-  ngOnInit() { }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
+    this.barChartType = this.barChartType;
+  }
 }
