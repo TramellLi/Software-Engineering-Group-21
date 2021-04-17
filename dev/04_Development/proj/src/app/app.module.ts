17497321950a96427eaa4e1c02d476206811473a
Wayne Guangzhe Wen <jt20452@bristol.ac.uk>
2021-04-17 12:38:40 +0100

    demo02 add service and css

--- dev/04_Development/proj/src/app/app.module.ts
+++ dev/04_Development/proj/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-
 import {ChartsModule} from 'ng2-charts';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BarChartComponent} from './bar-chart/bar-chart.component';
-
+import {DoughnutChartComponent} from './doughnut-chart/doughnut-chart.component';
+import {RadarChartComponent} from './radar-chart/radar-chart.component';
+import {PieChartComponent} from './pie-chart/pie-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarChartComponent
+    BarChartComponent,
+    DoughnutChartComponent,
+    RadarChartComponent,
+    PieChartComponent
   ],
   imports: [
     BrowserModule,
